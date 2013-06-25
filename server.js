d524633462ee98018e600886cee2c31485c266e4
Jonnie Spratley <jonniespratley@gmail.com>
2013-06-24 20:33:22 -0700

    added dist build

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var http = require("http"),
  
 http.createServer(function(request, response) {
  
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
+  var uri = url.parse(request.url).pathname,
+		filename = path.join(process.cwd()+'/dist', uri);
   
   path.exists(filename, function(exists) {
     if(!exists) {
